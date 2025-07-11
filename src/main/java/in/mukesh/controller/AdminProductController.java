0f38e2965f82c098cba7d77d8ca01ffa2ff1a8dc
Mukeswar <126387293+Mukeswar-143@users.noreply.github.com>
2025-07-11 03:48:52 -0700

    Update AdminProductController.java

--- src/main/java/in/mukesh/controller/AdminProductController.java
+++ src/main/java/in/mukesh/controller/AdminProductController.java
@@ -3,9 +3,7 @@ package in.mukesh.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import in.mukesh.entity.CartItemEntity;
 import in.mukesh.entity.ProductEntity;
-import in.mukesh.repository.CartItemRepository;
 import in.mukesh.service.ProductService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -25,12 +23,6 @@ public class AdminProductController {
 
     @Autowired
     private ProductService productService;
-<<<<<<< HEAD
-=======
-
-    @Autowired
-    private CartItemRepository cartRepo;
->>>>>>> 8f6f04d3fbba3fca572fc7a87e375a480f85a90a
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -62,16 +54,7 @@ public class AdminProductController {
             String adminUsername = SecurityContextHolder.getContext().getAuthentication().getName();
             product.setCreatedBy(adminUsername);
 
-<<<<<<< HEAD
             String result = productService.saveProductWithImage(product, imageFile);
-=======
-            File uploadDir = new File(IMAGE_UPLOAD_DIR);
-            if (!uploadDir.exists()) {
-                uploadDir.mkdirs();
-            }
-
-            String result = productService.saveProductWithImage(product, imageFile, IMAGE_UPLOAD_DIR);
->>>>>>> 8f6f04d3fbba3fca572fc7a87e375a480f85a90a
             return ResponseEntity.status(HttpStatus.CREATED).body(result);
 
         } catch (Exception e) {
@@ -92,8 +75,4 @@ public class AdminProductController {
 
         return ResponseEntity.ok(carts);
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> 8f6f04d3fbba3fca572fc7a87e375a480f85a90a
 }
